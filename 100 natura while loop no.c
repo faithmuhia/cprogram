3bb7cad335ebcfb2ed9cefbfeaedbdfa968f1897
faithmuhia <123807639+faithmuhia@users.noreply.github.com>
2023-02-23 15:21:59 +0300

    Add files via upload

---
+++ 100 natura while loop no.c
@@ -0,0 +1,12 @@
+//natural numbers
+#include <stdio.h>
+int main() {
+    int n=100;
+    while(n>=10){
+    printf("%d ",n);
+    n=n-1; 
+    }
+    
+    printf("for loop body finished");
+return 0;
+}
